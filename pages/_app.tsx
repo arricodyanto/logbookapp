e3564bf80ca878bfd6e7fdd82a337fe24ade7c0a
arricodyanto <arricohandyanto@gmail.com>
2022-06-10 17:45:18 +0700

    stak 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
+import '../styles/dashboard.css'
+import '../styles/core.css'
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Sistem Monitoring Tugas Akhir</title>
+        <meta name="description" content="An App to Monitoring Students with Logbook" />
+        <link rel="icon" href="/favicon.png" />
+
+        {/* <!-- Fonts --> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
+        {/* <!-- End fonts --> */}
+      
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
