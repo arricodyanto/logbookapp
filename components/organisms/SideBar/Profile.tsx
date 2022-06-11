96eae196e645136fb7d4caa1b29e5603de857ef8
arricodyanto <arricohandyanto@gmail.com>
2022-06-11 17:41:03 +0700

    patch v2.0

---
+++ components/organisms/SideBar/Profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <div className="user text-center pb-50 pe-30">
+        <img src="/img/avatar.png" width="90" height="90" className="img-fluid mb-20" />
+        <h2 className="fw-bold text-xl color-palette-1 m-0">User1</h2>
+        <p className="color-palette-2 m-0">user1@mail.com</p>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/dashboard.css'
-import '../styles/core.css'
-
+import '../styles/utilities.css'
+import '../styles/homepage.css'
+import '../styles/detail.css'
+import '../styles/checkout.css'
+import '../styles/complete-checkout.css'
+import '../styles/sign-in.css'
+import '../styles/sign-up.css'
+import '../styles/sign-up-photo.css'
+import '../styles/sign-up-success.css'
+import '../styles/404-not-found.css'
+import '../styles/sidebar.css'
+import '../styles/overview.css'
+import '../styles/transactions.css'
+import '../styles/transactions-detail.css'
+import '../styles/edit-profile.css'
+import '../styles/navbar-log-in.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
@@ -8,19 +21,31 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Sistem Monitoring Tugas Akhir</title>
-        <meta name="description" content="An App to Monitoring Students with Logbook" />
-        <link rel="icon" href="/favicon.png" />
+        {/* Bootstrap CSS */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"/>
+        
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
 
-        {/* <!-- Fonts --> */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-        {/* <!-- End fonts --> */}
+        {/* AOS Animation */}
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
       
+        {/* Call Bootstrap JS */}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous"/>
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.jpg" />
+
+        {/* Title Website */}
+        <title>Simota - Sistem Informaasi Monitoring Tugas Akhir</title>
       </Head>
       <Component {...pageProps} />
-    </>
+  </>
   );
 }
 
